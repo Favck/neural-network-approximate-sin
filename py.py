575e96b90e56bc21a64cde9e7b08e8493dd6c5b3
Никита <nikitagarkusha2006@gmail.com>
2026-05-08 11:26:31 +0300

    xxx

--- py.py
+++ py.py
@@ -1,148 +1,112 @@
 import numpy as np
-
-
-def predict(x):
-    W1 = np.load("W1.npy")
-    W2 = np.load("W2.npy")
-    b2 = np.load("b2.npy")
-    b1 = np.load("b1.npy")
-
-    z1 = W1 @ x + b1
-    a1 = 1/(1+np.exp(-z1))
-    pred = W2 @ a1 + b2
-    return pred
-
-class Layer_in:
-
-    def __init__(self):
-        self.W1 = np.random.normal(size=(3, 1))
-        self.b1 = np.random.normal(size=(3, 1))
-
-    def sigmoid(self, z):
-        return 1/(1+np.exp(-z))
-
-    def forward(self, x):
-        self.z1 = (self.W1 @ x).reshape(-1,1) + self.b1
-        np.save("z1.npy", self.z1)
-        self.a1 = self.sigmoid(self.z1)
-        np.save("a1.npy", self.a1)
-        return self.a1
-
-    def update_W1(self, W1):
-        self.W1 = W1
-        np.save("W1", self.W1)
-        return True
-
-    def update_b1(self, b1):
-        self.b1 = b1
-        np.save("b1", self.b1)
-
-
-class Layer1(Layer_in):
-
-    W2 = np.random.normal(size=(1, 3))
-    b2 = np.random.normal(size=(1, 1))
-
-
-    def sigmoid_derivatives(self, z):
-        return np.exp(-z)/((1 + np.exp(-z))**2)
-    
-    def forward(self):
-
-        self.z2 = self.W2 @ np.load("a1.npy") + self.b2
-        
-        np.save("z2.npy", self.z2)
-        return self.z2
-    
-    def backward(self, target):
-        der1 = (2*(np.load("z2.npy") - target) * self.W2.T)
-        
-        der2 = self.sigmoid_derivatives(np.load("z1.npy"))
-        der3 = der1 * der2
-
-        super().update_W1(1 - 0.001*(der3 @ np.load("a1.npy").T))
-        super().update_b1(der1)
-    
-    def update_W2(self, W2):
-        self.W2 = W2
-        np.save("W2", self.W2)
-
-    def update_b2(self, b2):
-        self.b2 = b2
-        np.save("b2", self.b2)
-        
-
-class Layer_out(Layer1):
+import matplotlib.pyplot as plt
+import torch
 
 
 
-    def out(self):
-        self.y = np.load("z2.npy")
-        
-        return self.y
+def forward_pass(x, W, b):
+    z = x @ W.T + b
 
-    def backward(self, target):
-        der1 = (2*(np.load("z2.npy") - target) @ np.load("a1.npy").T)
+    return z
 
-        super().update_W2(1 - 0.001*der1)
-        super().update_b2(2*(np.load("z2.npy") - target))
+def update_weight(w, b):
+    w.data -= 0.001*w.grad.data
+    b.data -= 0.001*b.grad.data
 
-    def loss(self, target):
-        L = (self.out() - target)**2
-        return L
+    w.grad.data.zero_()
+    b.grad.data.zero_()
+    return w, b
 
+def plot_sin(x, y, w, b, fig, ax, line_true, line_pred):
+    x_ = x.view(-1, 1)          
+    y_ = y.view(-1, 1)          
+
+    idx = torch.argsort(x_.squeeze())
+
+    with torch.no_grad():
+        x_norm = x_ / (3 * np.pi)   
+        z = forward_pass(x_norm, w[0], b[0])
+        a = torch.tanh(z)
+        y_pred = forward_pass(a, w[1], b[1])
+
+    x_np      = x_[idx].numpy()
+    y_np      = y_[idx].numpy()
+    y_pred_np = y_pred[idx].cpu().numpy()
+
+    line_true.set_data(x_np, y_np)
+    line_pred.set_data(x_np, y_pred_np)
+    ax.relim()
+    ax.autoscale_view()
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    plt.pause(0.01)
 
 
 if __name__=="__main__":
 
-    data = np.array([[x, np.sin(x)] for x in range(-1000, 1000)])
 
-    data_train = data[:1800]
-    data_test = data[1800:]
-
-    x_train = np.array([x[0] for x in data_train])
-    y_train = np.array([y[1] for y in data_train])
-
-    print(predict(np.array([0])))
-
-
-
-    # N = 2
-
-    # Lay_in = Layer_in()
-    # Lay1 = Layer1()
-    # Lay_out = Layer_out()
+    x_raw = torch.linspace(-3*np.pi, 3*np.pi, 5000, dtype=torch.float32)
+    x = x_raw/(3*np.pi)
+    y = torch.sin(x_raw)
     
-    # for epoch in range(N):
-    #     Loss = []
-    #     np.random.shuffle(x_train)
-    #     np.random.shuffle(y_train)
-    #     for i in range(0, data_train.shape[0]):
-    #         x_batch = x_train[i:i+32].reshape(-1, 1)
-    #         y_batch = y_train[i:i+32].reshape(-1, 1)
-    #         loss_batch = 0
-    #         for x, y in zip(x_batch, y_batch):
-    #             x = np.array([x])
-    #             y = np.array([y])
-    #             Lay_in.forward(x)
-    #             Lay1.forward()
-    #             Lay_out.out()
 
-    #             loss_batch += Lay_out.loss(y)
+    D_in, H, D_out = 1, 512, 1
 
-    #             Lay1.backward(y)
-    #             Lay_out.backward(y)
-    #         Loss.append(loss_batch)
-    #         print(f'Loss {epoch} epoch: {loss_batch/32}  sqrt: {np.sqrt(loss_batch/32)}')
+    w1 = torch.randn(H, D_in, requires_grad=True, dtype=torch.float32)
+    w2 = torch.randn(D_out, H, requires_grad=True, dtype=torch.float32)
+    b1 = torch.randn(H, requires_grad=True, dtype=torch.float32)
+    b2 = torch.randn(D_out,requires_grad=True, dtype=torch.float32)
 
-    
-    #     print(f'Loss {epoch} epoch: {sum(Loss)/len(Loss)}  sqrt: {np.sqrt(sum(Loss)/len(Loss))}')
+    Epoch = 2000
+    batch = 64
+    plt.ion()
+    fig, ax = plt.subplots()
+
+    line_true, = ax.plot([], [], 'b', label='true')
+    line_pred, = ax.plot([], [], 'r', label='pred')
+
+    ax.legend()
+    ax.set_xlim(-10*np.pi, 10*np.pi)
+    ax.set_ylim(-1.5, 1.5)
+    history = []
+    for epoch in range(Epoch):
+        
+        LOSS = 0.0
+        idx = torch.randperm(x.size(0))
+        x_shuf = x[idx]
+        y_shuf = y[idx]
+        for i in range(0, x.shape[0], batch):
+            x_batch = x_shuf[i:i+batch].view(-1,1)
+            y_batch = y_shuf[i:i+batch].view(-1,1)
             
+            
+            z1 = forward_pass(x_batch, w1, b1)
+            a1 = torch.tanh(z1)
 
+            y_pred = forward_pass(a1, w2, b2)
+            
+            loss = torch.mean((y_pred - y_batch)**2)
+            LOSS += loss.item()
+            history.append(loss.item())
+            loss.backward()
+
+            w1, b1 = update_weight(w1, b1)
+            w2, b2 = update_weight(w2, b2)
+
+            if i%2000 == 0:
+                plot_sin(x_raw, y, (w1, w2), (b1, b2), fig, ax, line_true, line_pred)
+
+
+
+        print(f"{epoch+1}/{Epoch} LOSS:{LOSS/(x.shape[0])}")
     
-
-
-
+    plt.ioff() 
+    plt.figure() 
+    plt.plot(history[:1000])
+    plt.xlabel("Batch")
+    plt.ylabel("Loss")
+    plt.title("Loss value")
+    plt.show()
 
 
 
